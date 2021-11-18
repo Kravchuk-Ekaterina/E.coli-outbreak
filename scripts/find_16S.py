a2936512e321ad38abb40442ecd7e9bf54f6e77e
Kravchuk-Ekaterina <kravchuk_ekaterina01@mail.ru>
2021-11-18 17:15:41 +0300

    Blast story

--- scripts/find_16S.py
+++ scripts/find_16S.py
@@ -3,3 +3,5 @@ with open('../assembly2/scaffolds.fasta') as scaffolds:
     seq = scaffolds.read()
 seq = ''.join(seq.split('\n')[1:])
 print(seq[291457:292994])
+print(seq[333038:334575])
+print(seq[906257:907794])
